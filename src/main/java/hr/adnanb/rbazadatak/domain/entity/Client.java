556173614b6ca88cc48febbc34ab2597a2ca9bdd
abrkovic <adnan.brkovic@sedamit.hr>
2025-05-08 18:59:09 +0200

    app init

---
+++ src/main/java/hr/adnanb/rbazadatak/domain/entity/Client.java
@@ -0,0 +1,68 @@
+package hr.adnanb.rbazadatak.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ime;
+    private String prezime;
+    private String oib;
+    private String statusKartice;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getOib() {
+        return oib;
+    }
+
+    public void setOib(String oib) {
+        this.oib = oib;
+    }
+
+    public String getStatusKartice() {
+        return statusKartice;
+    }
+
+    public void setStatusKartice(String statusKartice) {
+        this.statusKartice = statusKartice;
+    }
+}
